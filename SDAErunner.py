c1e7fc7b656ffebedc46e455d4ea2e5f760d7aa1
woshiljw <3222413873@qq.com>
2018-12-09 21:58:38 +0800

    Autoencoder_conv2deconv is completed(untest)

--- SDAErunner.py
+++ SDAErunner.py
@@ -7,9 +7,15 @@ from Data import Data
 
 ae1 = Autoencoder_conv2conv('ae1',
                             encoder_filter_size=[5,5,3,64],
-                            dencoder_filter_size=[1,1,64,3],
+                            decoder_filter_size=[1,1,64,3],
                             input_shape=[64,32,128,3],
                             optimizer=tf.train.AdamOptimizer(0.0000085))
+
+ae2 = Autoencoder_conv2deconv('ae2',
+                              encoder_filter_size=[3,3,64,96],
+                              decoder_filter_size=[5,5,64,96],
+                              input_shape=[64,32,128,64],
+                              optimizer=tf.train.AdamOptimizer(0.00001))
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -20,7 +26,8 @@ for epoch in range(20):
     total_batch = int(len(data.train_data) / 64)
     data.num = 0
     for i in range(total_batch):
-        _,cost = sess.run(ae1.partial_fit(), feed_dict={ae1.x: data.batch_size([-1, 32, 128, 3])})
+        ae1_output = sess.run(ae1.encode, feed_dict={ae1.x: data.batch_size([-1, 32, 128, 3])})
+        _,cost = sess.run(ae2.partial_fit(), feed_dict={ae2.x: ae1_output})
         avg_cost += cost / len(data.train_data) * 64
 
     print("Epoch:{},Cost:{:.9f}".format(epoch, avg_cost))
--- autoencoders/AutoEncoder.py
+++ autoencoders/AutoEncoder.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 
 #两个卷积层组成的自编码器
 class Autoencoder_conv2conv(object):
-    def __init__(self,name,encoder_filter_size,dencoder_filter_size
+    def __init__(self,name,encoder_filter_size,decoder_filter_size
                  ,input_shape,optimizer,
                  transfer_function=tf.nn.relu):
-        self.weight = self._initialize_weights(name,encoder_filter_size,dencoder_filter_size)
+        self.weight = self._initialize_weights(name,encoder_filter_size,decoder_filter_size)
         self.x = tf.placeholder(tf.float32,input_shape)
 
         #编码解码部分
@@ -25,7 +25,7 @@ class Autoencoder_conv2conv(object):
         self.cost = tf.reduce_mean(tf.square(tf.subtract(self.decode,self.x)))
         self.optimizer = optimizer.minimize(self.cost)
 
-    def _initialize_weights(self,name,encoder_filter_size,dencoder_filter_size):
+    def _initialize_weights(self,name,encoder_filter_size,decoder_filter_size):
         all_weights = dict()
         all_weights['w1'] = tf.get_variable(name=name + '_w1',
                                             shape=encoder_filter_size,
@@ -34,10 +34,10 @@ class Autoencoder_conv2conv(object):
                                             shape=[encoder_filter_size[3]],
                                             initializer=tf.contrib.layers.xavier_initializer(uniform=False))
         all_weights['w2'] = tf.get_variable(name=name + '_w2',
-                                            shape=dencoder_filter_size,
+                                            shape=decoder_filter_size,
                                             initializer=tf.contrib.layers.xavier_initializer(uniform=False))
         all_weights['b2'] = tf.get_variable(name=name + '_b2',
-                                            shape=[dencoder_filter_size[3]],
+                                            shape=[decoder_filter_size[3]],
                                             initializer=tf.contrib.layers.xavier_initializer(uniform=False))
         return all_weights
 
@@ -50,9 +50,53 @@ class Autoencoder_conv2conv(object):
 
 #卷积层与反卷积层组成的自编码器
 class Autoencoder_conv2deconv(object):
-    def __init__(self):
-        pass
+    def __init__(self,name,encoder_filter_size,decoder_filter_size,
+                 input_shape,optimizer,transfer_function=tf.nn.relu):
+        self.weight = self._initialize_weights(name,encoder_filter_size,decoder_filter_size)
+        self.x = tf.placeholder(tf.float32,input_shape)
 
+        #编码解码部分
+        self.maxpool = tf.nn.max_pool(self.x,[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+        self.encode = transfer_function(
+            tf.add(
+                tf.nn.conv2d(self.maxpool,self.weight['w1'],strides=[1,1,1,1],padding='SAME'),
+                self.weight['b1']
+            )
+        )
+
+        decoder_output_shape = [input_shape[0],input_shape[1]//2,input_shape[2]//2,input_shape[3]]
+        self.decode = transfer_function(
+            tf.add(
+                tf.nn.conv2d_transpose(self.encode,self.weight['w2'],decoder_output_shape,[1,1,1,1],padding='SAME'),
+                self.weight['b2']
+            )
+        )
+
+        self.upscale = tf.image.resize_nearest_neighbor(self.decode,input_shape[1:3])
+        self.cost = tf.reduce_mean(tf.square(tf.subtract(self.upscale, self.x)))
+        self.optimizer = optimizer.minimize(self.cost)
+
+    def _initialize_weights(self,name,encoder_filter_size,decoder_filter_size):
+        all_weights = dict()
+        all_weights['w1'] = tf.get_variable(name=name + '_w1',
+                                            shape=encoder_filter_size,
+                                            initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        all_weights['b1'] = tf.get_variable(name=name + '_b1',
+                                            shape=[encoder_filter_size[3]],
+                                            initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        all_weights['w2'] = tf.get_variable(name=name + '_w2',
+                                            shape=decoder_filter_size,
+                                            initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        all_weights['b2'] = tf.get_variable(name=name + '_b2',
+                                            shape=[decoder_filter_size[2]],
+                                            initializer=tf.contrib.layers.xavier_initializer(uniform=False))
+        return all_weights
+
+    def partial_fit(self):
+        return (self.optimizer,self.cost)
+
+    def total_cost(self):
+        return self.cost
 
 #全连接层与反卷积层组成的自编码器
 class Autoencoder_full2deconv(object):
