e8228122ccf5bfb9d190c32b88a8a6c1ee6f34fe
woshiljw <3222413873@qq.com>
2018-12-10 11:33:11 +0800

    remove upscale and maxpool from conv2deconv

--- SDAErunner.py
+++ SDAErunner.py
@@ -15,19 +15,19 @@ ae2 = Autoencoder_conv2deconv('ae2',
                               encoder_filter_size=[3,3,64,96],
                               decoder_filter_size=[5,5,64,96],
                               input_shape=[64,32,128,64],
-                              optimizer=tf.train.AdamOptimizer(0.00001))
+                              optimizer=tf.train.AdamOptimizer(0.0000095))
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 data = Data('../data/data.npz',64)
-for epoch in range(20):
+for epoch in range(2000):
     avg_cost = 0
     total_batch = int(len(data.train_data) / 64)
     data.num = 0
     for i in range(total_batch):
-        #ae1_output = sess.run(ae1.encode, feed_dict={ae1.x: data.batch_size([-1, 32, 128, 3])})
-        _,cost = sess.run(ae1.partial_fit(), feed_dict={ae1.x: data.batch_size([-1, 32, 128, 3])})
+        ae1_output = sess.run(ae1.encode, feed_dict={ae1.x: data.batch_size([-1, 32, 128, 3])})
+        _,cost = sess.run(ae2.partial_fit(), feed_dict={ae2.x: ae1_output})
         avg_cost += cost / len(data.train_data) * 64
 
     print("Epoch:{},Cost:{:.9f}".format(epoch, avg_cost))
--- autoencoders/AutoEncoder.py
+++ autoencoders/AutoEncoder.py
@@ -15,13 +15,9 @@ class Autoencoder_conv2conv(object):
                 self.weight['b1']
             )
         )
-
-        self.hidden_maxpool = tf.nn.max_pool(self.encode,[1,2,2,1],strides=[1,2,2,1],padding='SAME')
-        self.hidden_upscale = tf.image.resize_nearest_neighbor(self.hidden_maxpool,input_shape[1:3])
-
         self.decode = transfer_function(
             tf.add(
-                tf.nn.conv2d(self.hidden_upscale,self.weight['w2'],[1,1,1,1],padding="SAME"),
+                tf.nn.conv2d(self.encode,self.weight['w2'],[1,1,1,1],padding="SAME"),
                 self.weight['b2']
             )
         )
@@ -60,15 +56,15 @@ class Autoencoder_conv2deconv(object):
         self.x = tf.placeholder(tf.float32,input_shape)
 
         #编码解码部分
-        self.maxpool = tf.nn.max_pool(self.x,[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+        #self.maxpool = tf.nn.max_pool(self.x,[1,2,2,1],strides=[1,2,2,1],padding='SAME')
         self.encode = transfer_function(
             tf.add(
-                tf.nn.conv2d(self.maxpool,self.weight['w1'],strides=[1,1,1,1],padding='SAME'),
+                tf.nn.conv2d(self.x,self.weight['w1'],strides=[1,1,1,1],padding='SAME'),
                 self.weight['b1']
             )
         )
 
-        decoder_output_shape = [input_shape[0],input_shape[1]//2,input_shape[2]//2,input_shape[3]]
+        decoder_output_shape = [input_shape[0],input_shape[1],input_shape[2],input_shape[3]]
         self.decode = transfer_function(
             tf.add(
                 tf.nn.conv2d_transpose(self.encode,self.weight['w2'],decoder_output_shape,[1,1,1,1],padding='SAME'),
@@ -76,8 +72,8 @@ class Autoencoder_conv2deconv(object):
             )
         )
 
-        self.upscale = tf.image.resize_nearest_neighbor(self.decode,input_shape[1:3])
-        self.cost = tf.reduce_mean(tf.square(tf.subtract(self.upscale, self.x)))
+        #self.upscale = tf.image.resize_nearest_neighbor(self.decode,input_shape[1:3])
+        self.cost = tf.reduce_mean(tf.square(tf.subtract(self.decode, self.x)))
         self.optimizer = optimizer.minimize(self.cost)
 
     def _initialize_weights(self,name,encoder_filter_size,decoder_filter_size):
