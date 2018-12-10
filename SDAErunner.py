bb8bf3acf48986890ad762724dc484f7905a3db1
woshiljw <3222413873@qq.com>
2018-12-10 20:55:30 +0800

    test the conv2deconv

--- SDAErunner.py
+++ SDAErunner.py
@@ -1,36 +1,40 @@
 import numpy as np
 import tensorflow as tf
-from autoencoders.AutoEncoder import Autoencoder_conv2conv,Autoencoder_conv2deconv,Autoencoder_full2deconv
+from autoencoders.AutoEncoder import Autoencoder_conv2conv, Autoencoder_conv2deconv, Autoencoder_full2deconv
 from Data import Data
 
 
+sess = tf.Session()
+saver = tf.train.import_meta_graph('./weights_saver/ae1_model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./weights_saver'))
 
-ae1 = Autoencoder_conv2conv('ae1',
-                            encoder_filter_size=[5,5,3,64],
-                            decoder_filter_size=[1,1,64,3],
-                            input_shape=[64,32,128,3],
-                            optimizer=tf.train.AdamOptimizer(0.0000085))
+x = tf.placeholder(tf.float32,[64,32,128,3])
+h = tf.nn.relu(
+    tf.nn.conv2d(
+        x,sess.run('ae1_w1:0'),[1,1,1,1],padding='SAME'
+    )+sess.run('ae1_b1:0')
+)
 
 ae2 = Autoencoder_conv2deconv('ae2',
                               encoder_filter_size=[3,3,64,96],
                               decoder_filter_size=[5,5,64,96],
                               input_shape=[64,32,128,64],
-                              optimizer=tf.train.AdamOptimizer(0.0000095))
+                              optimizer=tf.train.AdamOptimizer(0.00001))
 
-sess = tf.Session()
+
+
 sess.run(tf.global_variables_initializer())
-saver = tf.train.Saver()
 
-data = Data('../data/data.npz',64)
+
+
+data = Data('../data/data.npz', 64)
 for epoch in range(2):
     avg_cost = 0
     total_batch = int(len(data.train_data) / 64)
     data.num = 0
     for i in range(total_batch):
-        _,cost = sess.run(ae1.partial_fit(), feed_dict={ae1.x: data.batch_size([-1, 32, 128, 3])})
+        input = sess.run(h,feed_dict={x:data.batch_size([-1, 32, 128, 3])})
+        _, cost = sess.run(ae2.partial_fit(), feed_dict={ae2.x: input})
         avg_cost += cost / len(data.train_data) * 64
 
     print("Epoch:{},Cost:{:.9f}".format(epoch, avg_cost))
-
-saver.save(sess,'./weights_saver/ae1_model')
-sess.close()
--- autoencoders/AutoEncoder.py
+++ autoencoders/AutoEncoder.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+from tensorflow.contrib.layers import batch_norm
 #两个卷积层组成的自编码器
 class Autoencoder_conv2conv(object):
     def __init__(self,name,encoder_filter_size,decoder_filter_size
@@ -55,15 +55,16 @@ class Autoencoder_conv2deconv(object):
         self.x = tf.placeholder(tf.float32,input_shape)
 
         #编码解码部分
-        #self.maxpool = tf.nn.max_pool(self.x,[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+        self.input = batch_norm(self.x)
+        self.maxpool = tf.nn.max_pool(self.input,[1,2,2,1],strides=[1,2,2,1],padding='SAME')
         self.encode = transfer_function(
             tf.add(
-                tf.nn.conv2d(self.x,self.weight['w1'],strides=[1,1,1,1],padding='SAME'),
+                tf.nn.conv2d(self.maxpool,self.weight['w1'],strides=[1,1,1,1],padding='SAME'),
                 self.weight['b1']
             )
         )
 
-        decoder_output_shape = [input_shape[0],input_shape[1],input_shape[2],input_shape[3]]
+        decoder_output_shape = [input_shape[0],input_shape[1]//2,input_shape[2]//2,input_shape[3]]
         self.decode = transfer_function(
             tf.add(
                 tf.nn.conv2d_transpose(self.encode,self.weight['w2'],decoder_output_shape,[1,1,1,1],padding='SAME'),
@@ -71,8 +72,9 @@ class Autoencoder_conv2deconv(object):
             )
         )
 
-        #self.upscale = tf.image.resize_nearest_neighbor(self.decode,input_shape[1:3])
-        self.cost = tf.reduce_mean(tf.square(tf.subtract(self.decode, self.x)))
+        self.upscale = tf.image.resize_nearest_neighbor(self.decode,input_shape[1:3])
+        self.upscale = batch_norm(self.upscale)
+        self.cost = tf.reduce_mean(tf.square(tf.subtract(self.upscale, self.input)))
         self.optimizer = optimizer.minimize(self.cost)
 
     def _initialize_weights(self,name,encoder_filter_size,decoder_filter_size):
