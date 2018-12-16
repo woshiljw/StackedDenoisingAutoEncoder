cc3d8ee340ec017b5e043de42a24f828bcec3a9e
woshiljw <3222413873@qq.com>
2018-12-16 16:18:45 +0800

    None

--- demo.py
+++ demo.py
@@ -1,17 +1,31 @@
 import tensorflow as tf
 import numpy as np
 from Data import Data
-from tensorflow.contrib.layers import batch_norm
 import cv2
-x = tf.placeholder(tf.float32, [64, 32, 128, 3])
-h = tf.nn.relu(
-batch_norm(
-    tf.nn.conv2d(x,tf.get_variable('w1',[5,5,3,64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),
-                 [1,1,1,1],padding='SAME')+tf.get_variable('b1',[64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False))
+from tensorflow.python.training import moving_averages
+def batch_normalization_layer(input):
+    axis = list(range(len(input.get_shape())-1))
+    mean,variance = tf.nn.moments(input,axis)
+    beta = tf.get_variable('beta',initializer=tf.zeros_initializer,shape=input.get_shape()[-1],dtype=tf.float32)
+    gamma = tf.get_variable('gamma',initializer=tf.ones_initializer,shape=input.get_shape()[-1],dtype=tf.float32)
 
-)
-)
-h = tf.nn.max_pool(h,[1,2,2,1],[1,2,2,1],padding='SAME')
+    moving_mean = tf.get_variable('moving_mean',initializer=tf.zeros_initializer,
+                                  shape=input.get_shape()[-1],dtype=tf.float32,trainable=False)
+    moving_variance = tf.get_variable('moving_var',initializer=tf.zeros_initializer,
+                                      shape=input.get_shape()[-1],dtype=tf.float32,trainable=False)
+    update_moving_mean = moving_averages.assign_moving_average(moving_mean,mean,0.999)
+    update_moving_variance = moving_averages.assign_moving_average(moving_variance,variance,0.999)
+
+    tf.add_to_collection('mean',update_moving_mean)
+    tf.add_to_collection('variance',update_moving_variance)
+
+    return tf.nn.batch_normalization(input,mean,variance,beta,gamma,0.001)
+
+x = tf.placeholder(tf.float32, [64, 32, 128, 3])
+h = tf.nn.conv2d(x,tf.get_variable('w1',[5,5,3,64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),
+                 [1,1,1,1],padding='SAME')+tf.get_variable('b1',[64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False))
+h = batch_normalization_layer(h)
+h = tf.nn.relu(h)
 # h = tf.nn.relu(
 #
 #     tf.nn.conv2d(h,tf.get_variable('w3',[3,3,64,96],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),
@@ -25,22 +39,22 @@ h = tf.nn.max_pool(h,[1,2,2,1],[1,2,2,1],padding='SAME')
 #                                bias_initializer=tf.contrib.layers.xavier_initializer(uniform=False),
 #                                padding='SAME'
 #                                )
-h = tf.nn.relu(
-batch_norm(
-    tf.nn.conv2d_transpose(
-        h,tf.get_variable('w4',[5,5,64,64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),[64,16,64,64],[1,1,1,1],
-        padding='SAME'
-    )+tf.get_variable('b4',[64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False))
-)
-)
-h = tf.nn.relu(
-batch_norm(
-    tf.nn.conv2d_transpose(
-        h,tf.get_variable('w5',[5,5,64,64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),[64,32,128,64],[1,2,2,1],
-        padding='SAME'
-    )+tf.get_variable('b5',[64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False))
-)
-)
+# h = tf.nn.relu(
+# batch_norm(
+#     tf.nn.conv2d_transpose(
+#         h,tf.get_variable('w4',[5,5,64,64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),[64,16,64,64],[1,1,1,1],
+#         padding='SAME'
+#     )+tf.get_variable('b4',[64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False))
+# )
+# )
+# h = tf.nn.relu(
+# batch_norm(
+#     tf.nn.conv2d_transpose(
+#         h,tf.get_variable('w5',[5,5,64,64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False)),[64,32,128,64],[1,2,2,1],
+#         padding='SAME'
+#     )+tf.get_variable('b5',[64],tf.float32,tf.contrib.layers.xavier_initializer(uniform=False))
+# )
+# )
 # h = tf.nn.dropout(h,0.75)
 #h = tf.nn.relu(batch_norm(h))
 out = tf.nn.relu(
